64fb18e789264aa02569da6df9a85bd2b0900680
Danish Khan <danish.khan@ebpearls.com>
2025-04-16 14:46:38 +0545

    Work on post list page design

--- client/src/routes/PostListPage.jsx
+++ client/src/routes/PostListPage.jsx
@@ -1,7 +1,29 @@
-const PostListPage = () => {
-  return (
-    <div className=''>PostListPage</div>
-  )
-}
+import { useState } from "react";
+import PostList from "../components/PostList";
+import SideMenu from "../components/SideMenu";
 
-export default PostListPage
+const PostListPage = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="">
+      <h1 className="mb-8 text-2xl">Development Blog</h1>
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className="bg-[#391600] text-sm text-white px-4 py-2 rounded-2xl mb-4 md:hidden"
+      >
+        {open ? "Close" : "Filter or Search"}
+      </button>
+      <div className="flex flex-col-reverse gap-8 md:flex-row justify-between">
+        <div className="">
+          <PostList />
+        </div>
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostListPage;
