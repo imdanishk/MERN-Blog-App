e83f5de42c7b89521f44ae45ea5c656a01e7b25c
Danish Khan <danish.khan@ebpearls.com>
2025-04-17 15:18:45 +0545

    setup clerk auth and webhook + post APIs

---
+++ backend/controllers/post.controller.js
@@ -0,0 +1,91 @@
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+export const getPost = async (req, res) => {
+  try {
+    const post = await Post.findOne({ slug: req.params.slug });
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+export const createPost = async (req, res) => {
+  try {
+    const clerkUserId = req.body.user;
+    
+    if (!clerkUserId) {
+      return res.status(401).json("Not authenticated!");
+    }
+
+    const user = await User.findOne({ clerkUserId });
+
+    if (!user) {
+      return res.status(404).json("User not found!");
+    }
+
+    const newPost = new Post({
+      user: user._id,
+      ...req.body,
+    });
+
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+export const deletePost = async (req, res) => {
+  try {
+    const clerkUserId = req.auth.userId;
+
+  if (!clerkUserId) {
+    return res.status(401).json("Not authenticated!");
+  }
+
+  const user = await User.findOne({ clerkUserId });
+
+  if (!user) {
+    return res.status(404).json("User not found!");
+  }
+
+  const post = await Post.findById(req.params.id);
+
+  if (!post) {
+    return res.status(404).json("Post not found!");
+  }
+
+  const deletedPost = await Post.findOneAndDelete({
+    _id: req.params.id,
+    user: user._id,
+  });
+
+  if (!deletedPost) {
+    return res.status(403).json("You can delete only your posts!");
+  }
+
+  res.status(200).json("Post has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+export const featurePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.body.id);
+    post.featured = !post.featured;
+    const updatedPost = await post.save();
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,5 +1,18 @@
-import express from "express"
+import express from "express";
+import {
+  getPosts,
+  getPost,
+  createPost,
+  deletePost,
+  featurePost,
+} from "../controllers/post.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-export default router
+router.get("/", getPosts);
+router.get("/:slug", getPost);
+router.post("/", createPost);
+router.delete("/:id", deletePost);
+router.patch("/feature", featurePost);
+
+export default router;
